5bb2d14a388f60480201555d81a741abd31c63a9
Mateusz Derks <mateusz.derks@toptal.com>
2017-01-09 22:41:14 +0100

    Add cache and replay for the web graph

--- index.js
+++ index.js
@@ -8,14 +8,28 @@ var shoe = require('shoe')
 var ecstatic = require('ecstatic')(__dirname + '/client')
 var through = require('through2')
 var xtend = require('xtend')
+var Cache = require('./cache')
 
 module.exports = function (opts) {
   opts = xtend(config.graf, opts)
+  var bufferSize = typeof opts.bufferSize !== 'undefined' ? opts.bufferSize : 50
 
+  var cache = Cache(bufferSize)
   var server = http.createServer(ecstatic).listen(opts.port)
   var ts = through()
 
+  if (cache.isEnabled()) {
+    ts.on('data', function (data) {
+      cache.push(data)
+    })
+  }
+
   var sock = shoe(function (stream) {
+    var history = cache.getValues()
+    history.forEach(function (value) {
+      stream.write(value)
+    })
+
     ts.pipe(stream)
   })
 
